38ca51abd4d526a9d444e7156874f56b89301bc9
chanchann <www.769440972@qq.com>
2020-12-20 18:18:34 +0800

    gdb

--- basic/smart_ptr/11_reset.cc
+++ basic/smart_ptr/11_reset.cc
@@ -1,10 +1,40 @@
 #include <iostream>
 #include <memory>
+#include <cassert>
 #include "./geek_chrono/make_unique.h"
-int main() {
+void test01() {
     auto a1 = make_unique<int>(10);
+    auto a2 = make_unique<int>(12);
     std::cout << *a1 << std::endl;
     *a1 = 11;
     std::cout << *a1 << std::endl;
+}
+
+void test02() {
+    auto a1 = make_unique<int>(10);
+    auto a2 = make_unique<int>(12);
+    std::cout << "a1 : " << a1.get() << " " << *a1 << std::endl;
+    std::cout << "a2 : " << a2.get() << " " << *a2 << std::endl;     
+    a2 = std::move(a1);
+    assert(a1 == nullptr);
+    std::cout << "a2 : " << a2.get() << " " << *a2 << std::endl;  
+}
+
+void test03() {
+    auto a1 = make_unique<int>(10);
+    auto a2 = make_unique<int>(12);
+    std::cout << "a1 : " << a1.get() << " " << *a1 << std::endl;
+    std::cout << "a2 : " << a2.get() << " " << *a2 << std::endl; 
+    a2.reset(a1.release());
+    assert(a1 == nullptr);
+
+    std::cout << "a2 : " << a2.get() << " " << *a2 << std::endl;  
+}
+int main() {
+    // test01();
+    // test02();
+    test03();
+
+
     return 0;
 }
