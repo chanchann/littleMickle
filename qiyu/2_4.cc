2317c171819b978f80a0271fbf435fa205005ad4
chanchann <www.769440972@qq.com>
2021-11-13 13:29:16 +0800

    update

--- qiyu/2_4.cc
+++ qiyu/2_4.cc
@@ -28,8 +28,29 @@ void Test()
 	TestForward(std::forward<int>(x));   // lvalue rvalue rvalue (important)
 	TestForward(std::forward<int &>(x));  // lvalue lvalue rvalue
 	TestForward(std::forward<int &&>(x));   // lvalue rvalue rvalue
-
 }
+
+// 解析:
+// 1. TestForward(1) 1为右值，
+// T&& -> int&&, 但是v是左值(以下皆是)，std::move(为右值)
+// printT(v) --> 左
+// std::forward<T>(v) --> int&& && --> int&&
+
+// 2. TestForward(x), x为左值
+// T&& -> int& 
+// std::forward<T>(v) int& && -> int& 左
+
+// 3. std::forward<int>(x)
+// int&& 
+// std::forward<T>(v) --> int&& && -> int&& 右
+
+// 4. std::forward<int &>(x)
+// int& && -> int&
+// std::forward<T>(v) int& && -> int& 左
+
+// std::forward<int &&>(x)
+// int&& && -> int&&
+// std::forward<T>(v) --> int&& && -> int&& 右
 
 int main(void)
 {
